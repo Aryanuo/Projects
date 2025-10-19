78faf2f3027bc418e605a394daaccc73e77e0265
aryan <aryanweb12@gmail.com>
2025-10-19 18:58:49 +0530

    higher_lower error fixed

--- Mini games/games/higher_lower.py
+++ Mini games/games/higher_lower.py
@@ -1,16 +1,17 @@
 import random
+def higher_lower(update_score):
+    player = int(input("Enter a no. between 1 to 100 "))
+    computer = random.randint(1,100)
+    count=1
 
-player = int(input("Enter a no. between 1 to 100 "))
-computer = random.randint(1,100)
-count=1
+    while(player!=computer):
+        if(player<computer):
+            print("Go higher")
+        else:
+            print("Go lower")
 
-while(player!=computer):
-    if(player<computer):
-        print("Go higher")
-    else:
-        print("Go lower")
+        count+=1
+        player = int(input("Again enter a no. between 1 to 100 "))
 
-    count+=1
-    player = int(input("Again enter a no. between 1 to 100 "))
-
-print(f"You won in {count} attempts")
+    print(f"You won in {count} attempts")
+    update_score("win")
